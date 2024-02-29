8c10a17b6fd595cbc56a30e24a8a6c7202fe8219
B-U-Games <159568542+B-U-Games@users.noreply.github.com>
2024-02-29 23:09:22 +0600

    the Dismay strelyalka

---
+++ Assets/Scenes/Fireball.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float damage = 10;
+    void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveFixedUpdate();
+        
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireball();
+    }
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.value -= damage;
+            if(enemyHealth.value <= 0)
+            {
+                Destroy(enemyHealth.gameObject);
+            }
+        }
+    }
+}
